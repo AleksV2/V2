50e812d70da4ccab4b89c5229fc922ac756a7f7d
Aleks <alana.king2000@gmail.com>
2018-02-26 11:27:50 +0000

    Example code stuff

--- Assets/Example Game/Scripts/Player.cs
+++ Assets/Example Game/Scripts/Player.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("hello world please kill me"); 
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(logtext);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string logtext = "kill me pls";
+
+    public float speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(logtext);
+        Rigidbody2D erecc = GetComponent<Rigidbody2D>();
+        erecc.velocity = Vector2.right*speed;
+        
+    }
 }

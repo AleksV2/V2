25e7528ac8b9ed37cc8593bfd0d92e90ea1f6918
Aleks <alana.king2000@gmail.com>
2018-03-05 10:48:08 +0000

    Added player controls for movement and jumping in example

--- Assets/Example Game/Scripts/Player.cs
+++ Assets/Example Game/Scripts/Player.cs
@@ -9,20 +9,67 @@ public class Player : MonoBehaviour
     {
         Debug.Log(logtext);
     }
+
     public string logtext = "kill me pls";
     public float health = 10;
     public float speed = 2;
-
-    
+    public float jumpspeed = 5;
+        
     // Update is called once per frame
     void Update()
     {
+        // Getting rigidbody from the game object we are attached to
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();        
+
+        // Number between 1 and -1 based on player pressing left or right
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // Boolean(t/f) based on player pressing spacebar
+        bool jump = Input.GetButtonDown("Jump");
+
+    // Find out if we are touching the ground
+
+        // Get collider attached to the object
+        Collider2D collider = GetComponent<Collider2D>();
+
+        //Find out if we are colliding with the ground
+        LayerMask groundlayer = LayerMask.GetMask("Ground");
+
+        bool touchingGround = collider.IsTouchingLayers(groundLayer);
+
         
-        Rigidbody2D erecc = GetComponent<Rigidbody2D>();
-        erecc.velocity = Vector2.right*speed;
+        //Debug.Log(horizontal);
+
+        // Cache a local copy of our rigidbody velocity
+        Vector2 velocity = rigidbody.velocity;
+
+        // Set the x component of the velocity based on input
+        velocity.x = horizontal*speed;
+
+        // Set y component of the velocity based on jump
+        if (jump == true && touchingGround == true)
+        {
+            velocity.y = jumpspeed;
+
+        }
+
+        // Set rigidbody velocity based on local copy
+        rigidbody.velocity = velocity;
 
 
-        
+
+        //Print a log when a mouse button is pressed
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Mouse button down");
+        }
+
+        //Print a log of the mouse position
+        Vector2 mousePosition = Input.mousePosition;
+        Debug.Log("mouse position is"+mousePosition);
+
+
+
     }
 
     public void ApplyDamage(float DamageToDeal)
